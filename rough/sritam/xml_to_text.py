fbb035edf276bb3b2d896b2081b4a0b92f24f331
sritambehera <sritambehera110@gmail.com>
2020-10-15 09:11:48 +0530

    temp-commit

--- rough/sritam/xml_to_text.py
+++ rough/sritam/xml_to_text.py
@@ -1,24 +1,41 @@
 import os
 import sys
+import xml.etree.ElementTree as ET
+
+
+def to_text(file):
+
+    mytree =ET.parse(file)
+    myroot =mytree.getroot()
+    print()
+    for word in myroot.iter():
+        print(word.tag)
 
 def list_files(startpath):
     for root,dir,files in os.walk(startpath):
-        if dir !='.git ':
-            for f in files:
-                print(f,end=" ")
+        for f in files:
+            print(f)
+            with open(root+'\\'+f,"rb") as data:
+                to_text(data)
+
+            break
+
+
 
 
 
 sys.stdout = open('output.txt', 'w')
 
 #printing all train files
-os.chdir('given_files/train_corpus')
+
+os.chdir('train_corpus')
+# print(os.getcwd())
 list_files(os.getcwd())
 
 print(" ")
+
 #printing all test files
 os.chdir('..')
 os.chdir('test_corpus')
 list_files(os.getcwd())
 
-# print (os.listdir())
